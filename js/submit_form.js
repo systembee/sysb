68d920810b06c0392328a8e0deb4c612c9c02fd8
Balbir Singh <balbir97@gmail.com>
2020-07-01 16:49:12 -0700

    Update submit_form.js

--- js/submit_form.js
+++ js/submit_form.js
@@ -13,7 +13,7 @@ beforeSend: function(){
 form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
 }
 }).done(function(data){
-form_status.html('<p class="text-success">Thank you for contacting us. We will contact you ASAP</p>').delay(3000).fadeOut();
+form_status.html('<p class="text-success">Thank you for contacting us. We will get back to you ASAP</p>').delay(3000).fadeOut();
 });
   
 $('#mc-embedded-subscribe-form')[0].reset();
